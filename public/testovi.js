2fcc981ba2447ec2a42ca74b7bf958a6eb068d3b
fpoljcic <fpoljcic1@etf.unsa.ba>
2020-01-19 22:26:04 +0100

    Popravljeni testovi sa spirale 2

--- public/testovi.js
+++ public/testovi.js
@@ -34,7 +34,7 @@ describe('Kalendar', function() {
 			assert.equal(brojac, 31, "Mjesec novembar ima 31 dan");
 		});
 
-		it('Pozivanje iscrtajKalendar za trenutni mjesec: očekivano je da je 1. dan u petak', function() {
+		it('Pozivanje iscrtajKalendar za trenutni mjesec: očekivano je da je 1. dan u srijedu', function() {
 			var kalendar = document.getElementById("kalendarRef");
 
 			Kalendar.iscrtajKalendar(kalendar, 10);
@@ -49,11 +49,11 @@ describe('Kalendar', function() {
 					break;
 			}
 
-			assert.equal(i, 2, "1. dan novembra se nalazi u 3. redu (indeks = 2)");
-			assert.equal(j, 4, "1. dan novembra se nalazi u 5. koloni (indeks = 4)");
+			assert.equal(i, 2, "1. dan januara se nalazi u 3. redu (indeks = 2)");
+			assert.equal(j, 6, "1. dan januara se nalazi u 7. koloni (indeks = 6)");
 		});
 
-		it('Pozivanje iscrtajKalendar za trenutni mjesec: očekivano je da je 30. dan u subotu', function() {
+		it('Pozivanje iscrtajKalendar za trenutni mjesec: očekivano je da je 30. dan u cetvrtak', function() {
 			var kalendar = document.getElementById("kalendarRef");
 
 			Kalendar.iscrtajKalendar(kalendar, 10);
@@ -68,8 +68,8 @@ describe('Kalendar', function() {
 					break;
 			}
 
-			assert.equal(i, 6, "30. dan novembra se nalazi u 7. redu (indeks = 6)");
-			assert.equal(j, 5, "30. dan novembra se nalazi u 6. koloni (indeks = 5)");
+			assert.equal(i, 7, "30. dan novembra se nalazi u 8. redu (indeks = 7)");
+			assert.equal(j, 0, "30. dan novembra se nalazi u 1. koloni (indeks = 0)");
 		});
 
 		it('Pozivanje iscrtajKalendar za januar: očekivano je da brojevi dana idu od 1 do 31 počevši od utorka', function() {
@@ -92,10 +92,10 @@ describe('Kalendar', function() {
 			}
 
 			assert.equal(redPrvog, 2, "1. dan januara se nalazi u 3. redu (indeks = 2)");
-			assert.equal(kolonaPrvog, 1, "1. dan januara se nalazi u 2. koloni (indeks = 1)");
+			assert.equal(kolonaPrvog, 2, "1. dan januara se nalazi u 3. koloni (indeks = 2)");
 		});
 
-		it('Moj test 1: Testiranje februara, ima 28 dana, zadnji je u četvrtak', function() {
+		it('Moj test 1: Testiranje februara, ima 29 dana, zadnji je u subotu', function() {
 			var kalendar = document.getElementById("kalendarRef");
 
 			Kalendar.iscrtajKalendar(kalendar, 1);
@@ -113,9 +113,9 @@ describe('Kalendar', function() {
 				}
 			}
 
-			assert.equal(redZadnjeg, 6, "28. dan februara se nalazi u 3. redu (indeks = 2)");
-			assert.equal(kolonaZadnjeg, 3, "28. dan februara se nalazi u 2. koloni (indeks = 1)");
-			assert.equal(brojac, 28, "Mjesec februar ima 28 dana");
+			assert.equal(redZadnjeg, 6, "28. dan februara se nalazi u 7. redu (indeks = 6)");
+			assert.equal(kolonaZadnjeg, 4, "28. dan februara se nalazi u 5. koloni (indeks = 4)");
+			assert.equal(brojac, 29, "Mjesec februar ima 29 dana");
 		});
 
 		it('Moj test 2: Testiranje rednog broja za svaki ponedjeljak u mjesecu julu', function() {
@@ -123,8 +123,8 @@ describe('Kalendar', function() {
 
 			Kalendar.iscrtajKalendar(kalendar, 6);
 
-			for (var i = 2; i < kalendar.rows.length - 1; i++) {
-				assert.equal(kalendar.rows[i].cells[0].innerHTML, 1 + (i - 2) * 7, (i - 1) + ". ponedjeljak se nalazi u " + (i + 1) + ". redu (indeks = " + i + "), 1. koloni (indeks = 0)");
+			for (var i = 3; i < kalendar.rows.length - 1; i++) {
+				assert.equal(kalendar.rows[i].cells[0].innerHTML, (i - 2) * 7 - 1, (i - 2) + ". ponedjeljak se nalazi u " + (i + 1) + ". redu (indeks = " + i + "), 1. koloni (indeks = 0)");
 			}
 		});
 	});
@@ -144,19 +144,19 @@ describe('Kalendar', function() {
 
 		it('Pozivanje obojiZauzeca gdje u zauzecima postoje duple vrijednosti za zauzeće istog termina: očekivano je da se dan oboji bez obzira što postoje duple vrijednosti', function() {
 			var kalendar = document.getElementById("kalendarRef");
-			var vanredna = [{datum: "01.11.2019", pocetak: "10:00", kraj: "12:00", naziv: "0-01", predavac: "Faris"},
-			{datum: "01.11.2019", pocetak: "10:00", kraj: "12:00", naziv: "0-01", predavac: "Armin"},
-			{datum: "28.11.2019", pocetak: "10:00", kraj: "12:00", naziv: "0-01", predavac: "Amra"},
-			{datum: "30.11.2019", pocetak: "10:00", kraj: "12:00", naziv: "0-01", predavac: "Sile"},
-			{datum: "30.11.2019", pocetak: "10:00", kraj: "12:00", naziv: "0-01", predavac: "Adin"}];
+			var vanredna = [{datum: "01.01.2020", pocetak: "10:00", kraj: "12:00", naziv: "0-01", predavac: "Faris"},
+			{datum: "01.01.2020", pocetak: "10:00", kraj: "12:00", naziv: "0-01", predavac: "Armin"},
+			{datum: "28.01.2020", pocetak: "10:00", kraj: "12:00", naziv: "0-01", predavac: "Amra"},
+			{datum: "30.01.2020", pocetak: "10:00", kraj: "12:00", naziv: "0-01", predavac: "Sile"},
+			{datum: "30.01.2020", pocetak: "10:00", kraj: "12:00", naziv: "0-01", predavac: "Adin"}];
 			Kalendar.ucitajPodatke(null, vanredna);
-			Kalendar.obojiZauzeca(kalendar, 10, "0-01", "10:00", "12:00");
+			Kalendar.obojiZauzeca(kalendar, 0, "0-01", "10:00", "12:00");
 
 			for (var i = 2; i < kalendar.rows.length; i++) {
 				for (var j = 0; j < kalendar.rows[i].cells.length; j++) {
 					if (kalendar.rows[i].cells[j].innerHTML == "" || kalendar.rows[i].cells[j].style.visibility != "visible")
 						continue;
-					if (i == 2 && j == 4 || i == 6 && j == 3 || i == 6 && j == 5)
+					if (i == 2 && j == 2 || i == 6 && j == 1 || i == 6 && j == 3)
 						assert.equal("zauzeta", kalendar.rows[i].cells[j].className, "Ćelije 1, 28 i 30 trebaju biti obojene u crvenu");
 					else
 						assert.equal("slobodna", kalendar.rows[i].cells[j].className, "Sve ćelije osim ćelija 1, 28 i 30 trebaju biti obojene u zelenu");
@@ -187,11 +187,11 @@ describe('Kalendar', function() {
 
 		it('Pozivanje obojiZauzece kada u podacima postoji zauzeće termina ali u drugom mjesecu: očekivano je da se ne oboji zauzeće', function() {
 			var kalendar = document.getElementById("kalendarRef");
-			var vanredna = [{datum: "01.12.2019", pocetak: "20:00", kraj: "22:00", naziv: "VA1", predavac: "Faris"},
-			{datum: "31.12.2019", pocetak: "20:00", kraj: "22:00", naziv: "VA1", predavac: "Armin"},
-			{datum: "02.03.2019", pocetak: "20:00", kraj: "22:00", naziv: "VA1", predavac: "Amra"},
-			{datum: "07.07.2019", pocetak: "20:00", kraj: "22:00", naziv: "VA1", predavac: "Sile"},
-			{datum: "07.11.2019", pocetak: "20:00", kraj: "22:00", naziv: "VA1", predavac: "Adin"}];
+			var vanredna = [{datum: "01.12.2020", pocetak: "20:00", kraj: "22:00", naziv: "VA1", predavac: "Faris"},
+			{datum: "31.12.2020", pocetak: "20:00", kraj: "22:00", naziv: "VA1", predavac: "Armin"},
+			{datum: "02.03.2020", pocetak: "20:00", kraj: "22:00", naziv: "VA1", predavac: "Amra"},
+			{datum: "07.07.2020", pocetak: "20:00", kraj: "22:00", naziv: "VA1", predavac: "Sile"},
+			{datum: "07.11.2020", pocetak: "20:00", kraj: "22:00", naziv: "VA1", predavac: "Adin"}];
 			Kalendar.ucitajPodatke(null, vanredna);
 			Kalendar.obojiZauzeca(kalendar, 10, "VA1", "20:00", "22:00");
 
@@ -199,7 +199,7 @@ describe('Kalendar', function() {
 				for (var j = 0; j < kalendar.rows[i].cells.length; j++) {
 					if (kalendar.rows[i].cells[j].innerHTML == "" || kalendar.rows[i].cells[j].style.visibility != "visible")
 						continue;
-					if (i == 3 && j == 3)
+					if (i == 3 && j == 5)
 						assert.equal("zauzeta", kalendar.rows[i].cells[j].className, "Ćelija 7 treba biti obojena u crvenu");
 					else
 						assert.equal("slobodna", kalendar.rows[i].cells[j].className, "Sve ćelije osim ćelije 7 trebaju biti obojene u zelenu");
@@ -229,9 +229,9 @@ describe('Kalendar', function() {
 
 		it('Dva puta uzastopno pozivanje obojiZauzece: očekivano je da boja zauzeća ostane ista', function() {
 			var kalendar = document.getElementById("kalendarRef");
-			var vanredna = [{datum: "01.11.2019", pocetak: "07:00", kraj: "09:00", naziv: "MA", predavac: "Faris"},
-			{datum: "21.11.2019", pocetak: "07:00", kraj: "09:00", naziv: "MA", predavac: "Armin"},
-			{datum: "30.11.2019", pocetak: "07:00", kraj: "09:00", naziv: "MA", predavac: "Amra"}];
+			var vanredna = [{datum: "01.01.2020", pocetak: "07:00", kraj: "09:00", naziv: "MA", predavac: "Faris"},
+			{datum: "21.01.2020", pocetak: "07:00", kraj: "09:00", naziv: "MA", predavac: "Armin"},
+			{datum: "30.01.2020", pocetak: "07:00", kraj: "09:00", naziv: "MA", predavac: "Amra"}];
 			var periodicna = [{dan: 0, semestar: "zimski", pocetak: "07:00", kraj: "09:00", naziv: "MA", predavac: "Vedran"}];
 			Kalendar.ucitajPodatke(periodicna, vanredna);
 			var obojeniX = [];
@@ -240,7 +240,7 @@ describe('Kalendar', function() {
 
 
 			// 1. poziv - PERIODICNA
-			Kalendar.obojiZauzeca(kalendar, 10, "MA", "07:00", "09:00");
+			Kalendar.obojiZauzeca(kalendar, 0, "MA", "07:00", "09:00");
 			for (var i = 2; i < kalendar.rows.length; i++) {
 				for (var j = 0; j < kalendar.rows[i].cells.length; j++) {
 					if (kalendar.rows[i].cells[j].innerHTML != "" && kalendar.rows[i].cells[j].style.visibility == "visible" && kalendar.rows[i].cells[j].className == "zauzeta") {
@@ -251,7 +251,7 @@ describe('Kalendar', function() {
 			}
 
 			// 2. poziv - PERIODICNA
-			Kalendar.obojiZauzeca(kalendar, 10, "MA", "07:00", "09:00");
+			Kalendar.obojiZauzeca(kalendar, 0, "MA", "07:00", "09:00");
 			for (var i = 2; i < kalendar.rows.length; i++) {
 				for (var j = 0; j < kalendar.rows[i].cells.length; j++) {
 					if (kalendar.rows[i].cells[j].innerHTML == "" || kalendar.rows[i].cells[j].style.visibility != "visible")
@@ -268,7 +268,7 @@ describe('Kalendar', function() {
 
 
 			// 1. poziv - VANREDNA
-			Kalendar.obojiZauzeca(kalendar, 10, "MA", "07:00", "09:00");
+			Kalendar.obojiZauzeca(kalendar, 0, "MA", "07:00", "09:00");
 			for (var i = 2; i < kalendar.rows.length; i++) {
 				for (var j = 0; j < kalendar.rows[i].cells.length; j++) {
 					if (kalendar.rows[i].cells[j].innerHTML != "" && kalendar.rows[i].cells[j].style.visibility == "visible" && kalendar.rows[i].cells[j].className == "zauzeta") {
@@ -279,7 +279,7 @@ describe('Kalendar', function() {
 			}
 
 			// 2. poziv - VANREDNA
-			Kalendar.obojiZauzeca(kalendar, 10, "MA", "07:00", "09:00");
+			Kalendar.obojiZauzeca(kalendar, 0, "MA", "07:00", "09:00");
 			for (var i = 2; i < kalendar.rows.length; i++) {
 				for (var j = 0; j < kalendar.rows[i].cells.length; j++) {
 					if (kalendar.rows[i].cells[j].innerHTML == "" || kalendar.rows[i].cells[j].style.visibility != "visible")
@@ -297,10 +297,10 @@ describe('Kalendar', function() {
 
 		it('Pozivanje ucitajPodatke, obojiZauzeca, ucitajPodatke - drugi podaci, obojiZauzeca: očekivano da se zauzeća iz prvih podataka ne ostanu obojena, tj. primjenjuju se samo posljednje učitani podaci', function() {
 			var kalendar = document.getElementById("kalendarRef");
-			var vanredna = [{datum: "01.02.2019", pocetak: "14:00", kraj: "14:30", naziv: "1-09", predavac: "Faris"},
-			{datum: "07.02.2019", pocetak: "14:00", kraj: "14:30", naziv: "1-09", predavac: "Armin"},
-			{datum: "20.02.2019", pocetak: "14:00", kraj: "14:30", naziv: "1-09", predavac: "Amra"},
-			{datum: "25.02.2019", pocetak: "14:00", kraj: "14:30", naziv: "1-09", predavac: "Sile"}];
+			var vanredna = [{datum: "01.02.2020", pocetak: "14:00", kraj: "14:30", naziv: "1-09", predavac: "Faris"},
+			{datum: "07.02.2020", pocetak: "14:00", kraj: "14:30", naziv: "1-09", predavac: "Armin"},
+			{datum: "20.02.2020", pocetak: "14:00", kraj: "14:30", naziv: "1-09", predavac: "Amra"},
+			{datum: "25.02.2020", pocetak: "14:00", kraj: "14:30", naziv: "1-09", predavac: "Sile"}];
 			Kalendar.ucitajPodatke(null, vanredna);
 			Kalendar.obojiZauzeca(kalendar, 1, "1-09", "14:00", "14:30");
 
@@ -309,17 +309,17 @@ describe('Kalendar', function() {
 				for (var j = 0; j < kalendar.rows[i].cells.length; j++) {
 					if (kalendar.rows[i].cells[j].innerHTML == "" || kalendar.rows[i].cells[j].style.visibility != "visible")
 						continue;
-					if (i == 2 && j == 4 || i == 3 && j == 3 || i == 5 && j == 2 || i == 6 && j == 0)
+					if (i == 2 && j == 5 || i == 3 && j == 4 || i == 5 && j == 3 || i == 6 && j == 1)
 						assert.equal("zauzeta", kalendar.rows[i].cells[j].className, "Ćelije 1, 7, 20 i 25 trebaju biti obojene u crvenu");
 					else
 						assert.equal("slobodna", kalendar.rows[i].cells[j].className, "Sve ćelije osim ćelija 1, 7, 20 i 25 trebaju biti obojene u zelenu");
 				}
 			}
 
-			vanredna = [{datum: "11.02.2019", pocetak: "14:00", kraj: "14:30", naziv: "1-09", predavac: "Faris"},
-			{datum: "14.02.2019", pocetak: "14:00", kraj: "14:30", naziv: "1-09", predavac: "Armin"},
-			{datum: "26.02.2019", pocetak: "14:00", kraj: "14:30", naziv: "1-09", predavac: "Amra"},
-			{datum: "27.02.2019", pocetak: "14:00", kraj: "14:30", naziv: "1-09", predavac: "Sile"}];
+			vanredna = [{datum: "11.02.2020", pocetak: "14:00", kraj: "14:30", naziv: "1-09", predavac: "Faris"},
+			{datum: "14.02.2020", pocetak: "14:00", kraj: "14:30", naziv: "1-09", predavac: "Armin"},
+			{datum: "26.02.2020", pocetak: "14:00", kraj: "14:30", naziv: "1-09", predavac: "Amra"},
+			{datum: "27.02.2020", pocetak: "14:00", kraj: "14:30", naziv: "1-09", predavac: "Sile"}];
 			Kalendar.ucitajPodatke(null, vanredna);
 			Kalendar.obojiZauzeca(kalendar, 1, "1-09", "14:00", "14:30");
 
@@ -328,7 +328,7 @@ describe('Kalendar', function() {
 				for (var j = 0; j < kalendar.rows[i].cells.length; j++) {
 					if (kalendar.rows[i].cells[j].innerHTML == "" || kalendar.rows[i].cells[j].style.visibility != "visible")
 						continue;
-					if (i == 4 && j == 0 || i == 4 && j == 3 || i == 6 && j == 1 || i == 6 && j == 2)
+					if (i == 4 && j == 1 || i == 4 && j == 4 || i == 6 && j == 2 || i == 6 && j == 3)
 						assert.equal("zauzeta", kalendar.rows[i].cells[j].className, "Ćelije 11, 14, 26 i 27 trebaju biti obojene u crvenu");
 					else
 						assert.equal("slobodna", kalendar.rows[i].cells[j].className, "Sve ćelije osim ćelija 11, 14, 26 i 27 trebaju biti obojene u zelenu");
@@ -338,13 +338,13 @@ describe('Kalendar', function() {
 
 		it('Moj test 1: Test intervala, filtriranje od 10:00 do 14:00, zauzeće 1 stavljeno od 12:00 do 14:00, zauzeće 2 od 14:00 do 16:00', function() {
 			var kalendar = document.getElementById("kalendarRef");
-			var vanredna = [{datum: "05.11.2019", pocetak: "12:00", kraj: "14:00", naziv: "1-08", predavac: "Faris"},
-							{datum: "06.11.2019", pocetak: "14:00", kraj: "16:00", naziv: "1-08", predavac: "Amir"}];
+			var vanredna = [{datum: "05.11.2020", pocetak: "12:00", kraj: "14:00", naziv: "1-08", predavac: "Faris"},
+							{datum: "06.11.2020", pocetak: "14:00", kraj: "16:00", naziv: "1-08", predavac: "Amir"}];
 			Kalendar.ucitajPodatke(null, vanredna);
 			Kalendar.obojiZauzeca(kalendar, 10, "1-08", "10:00", "14:00");
 
-			assert.equal("zauzeta", kalendar.rows[3].cells[1].className, "Zauzeće 1 treba biti prikazano (boja crvena)");
-			assert.equal("slobodna", kalendar.rows[3].cells[2].className, "Zauzeće 2 ne treba biti prikazano (boja zelena)");
+			assert.equal("zauzeta", kalendar.rows[3].cells[3].className, "Zauzeće 1 treba biti prikazano (boja crvena)");
+			assert.equal("slobodna", kalendar.rows[3].cells[4].className, "Zauzeće 2 ne treba biti prikazano (boja zelena)");
 		});
 
 		it('Moj test 2: Testiranje filtriranja po sali, 5 zauzeća u različitim salama, filtrirat ćemo jednu', function() {
